a620aeff3aa249394242c667520244875475453a
matbarr <matibarra@yahoo.com>
2026-08-12 18:23:19 -0600

    Initial commit
    setup inicial

---
+++ MediCare/src/test/java/com/MediCare/MatiasBarrantes/MediCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MediCare.MatiasBarrantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
